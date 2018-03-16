edfe9a38ab0e3d119df3bd63cbdbebd7b46e815d
Christoffer Jansson <CTJansson@gmail.com>
2018-03-16 08:28:48 +0100

    Initial Commit

---
+++ PasswordGen/PasswordGen/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace PasswordGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.checkBoxLowercase = new System.Windows.Forms.CheckBox();
+            this.checkBoxUppercase = new System.Windows.Forms.CheckBox();
+            this.checkBoxNumbers = new System.Windows.Forms.CheckBox();
+            this.checkBoxSymbols = new System.Windows.Forms.CheckBox();
+            this.checkBoxSwedish = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PasswordLength = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnClipboard = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.PasswordLength.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(32, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(115, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "16 - Characters";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(32, 45);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(115, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "32 - Characters";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(32, 71);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(115, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "48 - Characters";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.Location = new System.Drawing.Point(32, 97);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(115, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "64 - Characters";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLowercase
+            // 
+            this.checkBoxLowercase.AutoSize = true;
+            this.checkBoxLowercase.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxLowercase.Location = new System.Drawing.Point(6, 19);
+            this.checkBoxLowercase.Name = "checkBoxLowercase";
+            this.checkBoxLowercase.Size = new System.Drawing.Size(166, 20);
+            this.checkBoxLowercase.TabIndex = 5;
+            this.checkBoxLowercase.Text = "Lowercase letters - a - z";
+            this.checkBoxLowercase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxUppercase
+            // 
+            this.checkBoxUppercase.AutoSize = true;
+            this.checkBoxUppercase.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxUppercase.Location = new System.Drawing.Point(6, 45);
+            this.checkBoxUppercase.Name = "checkBoxUppercase";
+            this.checkBoxUppercase.Size = new System.Drawing.Size(171, 20);
+            this.checkBoxUppercase.TabIndex = 7;
+            this.checkBoxUppercase.Text = "Uppercase letters - A - Z";
+            this.checkBoxUppercase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxNumbers
+            // 
+            this.checkBoxNumbers.AutoSize = true;
+            this.checkBoxNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxNumbers.Location = new System.Drawing.Point(6, 71);
+            this.checkBoxNumbers.Name = "checkBoxNumbers";
+            this.checkBoxNumbers.Size = new System.Drawing.Size(116, 20);
+            this.checkBoxNumbers.TabIndex = 8;
+            this.checkBoxNumbers.Text = "Numbers - 0 - 9";
+            this.checkBoxNumbers.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSymbols
+            // 
+            this.checkBoxSymbols.AutoSize = true;
+            this.checkBoxSymbols.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxSymbols.Location = new System.Drawing.Point(6, 97);
+            this.checkBoxSymbols.Name = "checkBoxSymbols";
+            this.checkBoxSymbols.Size = new System.Drawing.Size(146, 20);
+            this.checkBoxSymbols.TabIndex = 9;
+            this.checkBoxSymbols.Text = "Symbols - ! \" # ¤ % & /";
+            this.checkBoxSymbols.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSwedish
+            // 
+            this.checkBoxSwedish.AutoSize = true;
+            this.checkBoxSwedish.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxSwedish.Location = new System.Drawing.Point(6, 123);
+            this.checkBoxSwedish.Name = "checkBoxSwedish";
+            this.checkBoxSwedish.Size = new System.Drawing.Size(161, 20);
+            this.checkBoxSwedish.TabIndex = 10;
+            this.checkBoxSwedish.Text = "Swedish letters - Å Ä Ö";
+            this.checkBoxSwedish.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Generate password";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(597, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "Password will appear here.";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PasswordLength
+            // 
+            this.PasswordLength.Controls.Add(this.radioButton5);
+            this.PasswordLength.Controls.Add(this.radioButton1);
+            this.PasswordLength.Controls.Add(this.radioButton2);
+            this.PasswordLength.Controls.Add(this.radioButton3);
+            this.PasswordLength.Controls.Add(this.radioButton4);
+            this.PasswordLength.Location = new System.Drawing.Point(208, 12);
+            this.PasswordLength.Name = "PasswordLength";
+            this.PasswordLength.Size = new System.Drawing.Size(171, 156);
+            this.PasswordLength.TabIndex = 13;
+            this.PasswordLength.TabStop = false;
+            this.PasswordLength.Text = "Password Length:";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton5.Location = new System.Drawing.Point(32, 123);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(71, 20);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.Text = "Custom";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.ActivateNumericControl);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBoxLowercase);
+            this.groupBox2.Controls.Add(this.checkBoxUppercase);
+            this.groupBox2.Controls.Add(this.checkBoxNumbers);
+            this.groupBox2.Controls.Add(this.checkBoxSymbols);
+            this.groupBox2.Controls.Add(this.checkBoxSwedish);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(190, 156);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Password Definitions";
+            // 
+            // btnClipboard
+            // 
+            this.btnClipboard.Location = new System.Drawing.Point(385, 46);
+            this.btnClipboard.Name = "btnClipboard";
+            this.btnClipboard.Size = new System.Drawing.Size(230, 23);
+            this.btnClipboard.TabIndex = 15;
+            this.btnClipboard.Text = "Copy to Clipboard";
+            this.btnClipboard.UseVisualStyleBackColor = true;
+            this.btnClipboard.Click += new System.EventHandler(this.btnClipboard_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(240, 174);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(139, 20);
+            this.numericUpDown1.TabIndex = 16;
+            this.numericUpDown1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 306);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnClipboard);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.PasswordLength);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PasswordLength.ResumeLayout(false);
+            this.PasswordLength.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.CheckBox checkBoxLowercase;
+        private System.Windows.Forms.CheckBox checkBoxUppercase;
+        private System.Windows.Forms.CheckBox checkBoxNumbers;
+        private System.Windows.Forms.CheckBox checkBoxSymbols;
+        private System.Windows.Forms.CheckBox checkBoxSwedish;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox PasswordLength;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnClipboard;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ PasswordGen/PasswordGen/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int checkBoxCount = 0;
+            Random random = new Random();
+
+            // Determine password length
+
+            short passwordLength = 0;
+            string length = "";
+
+            foreach (Control control in PasswordLength.Controls)
+            {
+                RadioButton radioButton = control as RadioButton;
+                if (radioButton.Checked)
+                {
+                    if (radioButton.Text == "Custom")
+                        length = numericUpDown1.Value.ToString();
+                    else
+                        length = radioButton.Text.Substring(0, 2);
+                }                   
+            }
+
+            short.TryParse(length, out passwordLength);
+
+            // Confirming Password definitions
+
+            string characters = "";
+
+            if (checkBoxLowercase.Checked)
+            {
+                int num = 0;
+                for (int i = 0; i < 26; i++)
+                {
+                    char letter = (char)('a' + num);
+                    characters += letter;
+                    num++;
+                }
+
+                checkBoxCount++;
+            }
+
+            if (checkBoxUppercase.Checked)
+            {
+                int num = 0;
+                for (int i = 0; i < 26; i++)
+                {
+                    char letter = (char)('A' + num);
+                    characters += letter;
+                    num++;
+                }
+
+                checkBoxCount++;
+            }
+
+            if (checkBoxNumbers.Checked)
+            {
+                int[] array = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+                for (int i = 0; i < array.Length; i++)
+                    characters += array[i];
+
+                checkBoxCount++;
+            }
+
+            if (checkBoxSymbols.Checked)
+            {
+                char[] array = new char[] { '!', '"', '#', '¤', '%', '&', '/', '(', ')', '=', '?', '^', '*', '-', '{', '}', '<', '>' };
+                for (int i = 0; i < array.Length; i++)
+                    characters += array[i];
+
+                checkBoxCount++;
+            }
+
+            if (checkBoxSwedish.Checked)
+            {
+                characters += "åÅäÄöÖ";
+
+                checkBoxCount++;
+            }
+
+            if (checkBoxCount > 0)
+            {
+                string password = "";
+                int index = 0;
+
+                for (int i = 0; i < passwordLength; i++)
+                {
+                    index = random.Next(0, characters.Length);
+                    char letter = characters[index];
+                    password += letter;
+                }
+
+                textBox1.Text = password;
+            }
+           
+
+        }
+
+        private void btnClipboard_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void ActivateNumericControl(object sender, EventArgs e)
+        {
+            if (radioButton5.Checked)
+                numericUpDown1.Visible = true;
+            else
+                numericUpDown1.Visible = false;
+        }
+    }
+}
